21b40f79a4895d973e724a2b0da9db9caa197e33
adammetal <adam.metal@codecool.com>
2024-03-13 10:07:10 +0100

    events reloaded

--- events.js
+++ events.js
@@ -2,41 +2,24 @@ const buttonA = document.querySelector("#button-a");
 const buttonB = document.querySelector("#button-b");
 const result = document.querySelector("#result");
 
-function handleButtonAClickEvent() {
+function print(message) {
   const ts = Date.now();
-
-  // trick
   const line = document.createElement("div");
-  line.innerText = "Button A Clicked!!!! " + ts;
+  line.innerText = ts + ": " + message;
   result.append(line);
 }
 
-function handleButtonAMouseEnterEvent() {
-  const ts = Date.now();
-
-  // trick
-  const line = document.createElement("div");
-  line.innerText = "Button A Mouse Entered" + ts;
-  result.append(line);
+function handleButtonAClickEvent() {
+  print("Btton A Clicked");
 }
 
 function handleButtonBClickEvent() {
-  const ts = Date.now();
-
-  // trick
-  const line = document.createElement("div");
-  line.innerText = "Button B Clicked!!!! " + ts;
-  result.append(line);
+  print("Button B Clicked");
 }
 
 buttonA.addEventListener("click", handleButtonAClickEvent);
 buttonB.addEventListener("click", handleButtonBClickEvent);
-buttonA.addEventListener("mouseenter", handleButtonAMouseEnterEvent);
-window.addEventListener("load", () => {
-  const ts = Date.now();
 
-  // trick
-  const line = document.createElement("div");
-  line.innerText = "Window loaded" + ts;
-  result.append(line);
+window.addEventListener("load", () => {
+  print("Window loaded");
 });
